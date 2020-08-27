0862b453d3985aa21aebae429b2c52dcc43e1283
David Kirby <david@ecomatics.net>
2020-08-27 12:01:44 -0400

    Parses JSON results from archive.org (see console)

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import './scss/base.scss';
 import './scss/typography.scss';
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    console.log(fetch('/dist/metadata/catalog.json').then(data => data.json()))
+});
