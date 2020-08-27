5ac41c7c24f19b195df22ec960e6e3818e8ef9ca
David Kirby <david@ecomatics.net>
2020-08-27 12:37:27 -0400

    Updates metadata path

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import './scss/base.scss';
 import './scss/typography.scss';
 
 document.addEventListener("DOMContentLoaded", function(event) { 
-    console.log(fetch('/dist/metadata/catalog.json').then(data => data.json()))
+    console.log(fetch('metadata/catalog.json').then(data => data.json()))
 });
